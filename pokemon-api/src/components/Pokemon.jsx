d0e4885d44c27bd68e1801fb98f53d621696db33
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-11 20:41:17 -0800

    Added a display button to Pokemon Functional Component

--- pokemon-api/src/components/Pokemon.jsx
+++ pokemon-api/src/components/Pokemon.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 export default (props) => {
     const [pokemon , setPokemon] = useState([]);
+    const [pokeCatch , setPokeCatch] = useState(false);
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
@@ -12,18 +13,26 @@ export default (props) => {
 
     console.log(pokemon)
 
+    const pokeGo = () => {
+        setPokeCatch(!pokeCatch);
+    }
+
     return(
         <div className="pokemon">
             <h1>Pokemon From index 1 to 807</h1>
-            <ul>
-                <ol>
-                    {
-                        pokemon.map( (poke, i) =>
-                            <li>{poke.name}</li>
-                        )
-                    }
-                </ol>
-            </ul>
+            {
+                pokeCatch ?
+                <ul>
+                    <ol>
+                        {
+                            pokemon.map( (poke, i) =>
+                                <li>{poke.name}</li>
+                            )
+                        }
+                    </ol>
+                </ul>
+                : <button onClick={ e => pokeGo() }>Fetch Pokemon</button>
+            }
         </div>
     )
 }
